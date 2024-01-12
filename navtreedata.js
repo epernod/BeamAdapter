cb7ff1388504303803320a2ddfe70b0e6a905474
epernod <erik.pernod@gmail.com>
2024-01-12 11:27:46 +0000

    Deploying to gh-pages from  @ 67678b8d9554c878ce3308b124da5802a7361f06 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -38,6 +38,65 @@ var NAVTREE =
         [ "Complex scenario", "index.html#autotoc_md10", null ]
       ] ]
     ] ],
+    [ "theory", "md_doc_modeling_theory.html", [
+      [ "Modeling Theory", "md_doc_modeling_theory.html#autotoc_md11", [
+        [ "1. Corotational beam model", "md_doc_modeling_theory.html#autotoc_md12", null ],
+        [ "2. B-splines , Bézier Splines", "md_doc_modeling_theory.html#autotoc_md13", null ]
+      ] ],
+      [ "Implementation", "md_doc_modeling_theory.html#autotoc_md14", [
+        [ "WireRestShape", "md_doc_modeling_theory.html#autotoc_md15", null ],
+        [ "WireBeamInterpolation", "md_doc_modeling_theory.html#autotoc_md16", null ],
+        [ "AdaptiveBeamForceFieldAndMass", "md_doc_modeling_theory.html#autotoc_md17", null ],
+        [ "AdaptiveBeamMapping", "md_doc_modeling_theory.html#autotoc_md18", null ]
+      ] ],
+      [ "References", "md_doc_modeling_theory.html#autotoc_md19", null ]
+    ] ],
+    [ "Scene Implementation", "md_doc_modeling_implementation.html", [
+      [ "All BeamAdapter components", "md_doc_modeling_implementation.html#autotoc_md21", [
+        [ "1. The Components on Root Node", "md_doc_modeling_implementation.html#autotoc_md22", [
+          [ "RequiredPlugin", "md_doc_modeling_implementation.html#autotoc_md23", null ],
+          [ "VisualStyle", "md_doc_modeling_implementation.html#autotoc_md24", null ],
+          [ "FreeMotionAnimationLoop", "md_doc_modeling_implementation.html#autotoc_md25", null ],
+          [ "LCPConstraintSolver", "md_doc_modeling_implementation.html#autotoc_md26", null ],
+          [ "CollisionPipeline", "md_doc_modeling_implementation.html#autotoc_md27", null ],
+          [ "BruteForceDetection", "md_doc_modeling_implementation.html#autotoc_md28", null ],
+          [ "LocalMinDistance", "md_doc_modeling_implementation.html#autotoc_md29", null ],
+          [ "CollisionResponse", "md_doc_modeling_implementation.html#autotoc_md30", null ],
+          [ "CollisionGroup", "md_doc_modeling_implementation.html#autotoc_md31", null ]
+        ] ],
+        [ "2. The Components for the shape of the catheter", "md_doc_modeling_implementation.html#autotoc_md32", [
+          [ "SteerableCatheter", "md_doc_modeling_implementation.html#autotoc_md33", null ],
+          [ "EdgeSetTopologyContainer", "md_doc_modeling_implementation.html#autotoc_md34", null ],
+          [ "EdgeSetTopologyModifier, EdgeSetGeometryAlgorithms", "md_doc_modeling_implementation.html#autotoc_md35", null ],
+          [ "MechanicalObject", "md_doc_modeling_implementation.html#autotoc_md36", null ]
+        ] ],
+        [ "3. The Components for simulation of the catheter", "md_doc_modeling_implementation.html#autotoc_md37", [
+          [ "EulerImplicit", "md_doc_modeling_implementation.html#autotoc_md38", null ],
+          [ "BTDLinearSolver", "md_doc_modeling_implementation.html#autotoc_md39", null ],
+          [ "RegularGrid", "md_doc_modeling_implementation.html#autotoc_md40", null ],
+          [ "MechanicalObject", "md_doc_modeling_implementation.html#autotoc_md41", null ],
+          [ "WireBeamInterpolation", "md_doc_modeling_implementation.html#autotoc_md42", null ],
+          [ "AdaptiveBeamForceFieldAndMass", "md_doc_modeling_implementation.html#autotoc_md43", null ],
+          [ "InterventionalRadiologyController", "md_doc_modeling_implementation.html#autotoc_md44", null ],
+          [ "LinearSolverConstraintCorrection", "md_doc_modeling_implementation.html#autotoc_md45", null ],
+          [ "FixedConstraint", "md_doc_modeling_implementation.html#autotoc_md46", null ]
+        ] ],
+        [ "4. Collision", "md_doc_modeling_implementation.html#autotoc_md47", [
+          [ "EdgeSetTopologyContainer, EdgeSetTopologyModifier", "md_doc_modeling_implementation.html#autotoc_md48", null ],
+          [ "MechanicalObject", "md_doc_modeling_implementation.html#autotoc_md49", null ],
+          [ "MultiAdaptiveBeamMapping", "md_doc_modeling_implementation.html#autotoc_md50", null ],
+          [ "Point / Line", "md_doc_modeling_implementation.html#autotoc_md51", null ]
+        ] ],
+        [ "5. Visualization", "md_doc_modeling_implementation.html#autotoc_md52", [
+          [ "Topology Components: QuadSetTopologyContainer, QuadSetTopologyModifier, etc...", "md_doc_modeling_implementation.html#autotoc_md53", null ],
+          [ "MechanicalObject", "md_doc_modeling_implementation.html#autotoc_md54", null ],
+          [ "AdaptiveBeamMapping", "md_doc_modeling_implementation.html#autotoc_md55", null ],
+          [ "OglModel", "md_doc_modeling_implementation.html#autotoc_md56", null ],
+          [ "IdentityMapping", "md_doc_modeling_implementation.html#autotoc_md57", null ]
+        ] ]
+      ] ],
+      [ "References", "md_doc_modeling_implementation.html#autotoc_md58", null ]
+    ] ],
     [ "Todo List", "todo.html", null ],
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
